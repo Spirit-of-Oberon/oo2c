09364c58ac588f9b5d4d22ab20a8875d43d85a0a
Michael van Acken <mva@users.sf.net>
2000-09-05 18:45:01 +0200

    *** empty log message ***

--- tests/symtab/wf-names-only.sh
+++ tests/symtab/wf-names-only.sh
@@ -4,6 +4,7 @@ err_count=0
 for i in wf/src/*.Mod; do
   module=`basename $i .Mod`
   rm -f "wf/doc/xml/${module}_symtab.xml"
+  #echo $TEST_PROG -r wf --names-only-xml "$module"
   if $TEST_PROG -r wf --names-only-xml "$module" >/dev/null; then
     if $DIFF "wf/out-xml/names-only/${module}_symtab.xml" "wf/doc/xml/${module}_symtab.xml"; then
       echo "ok: $i"
