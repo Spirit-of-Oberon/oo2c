0252f1748270b6d60e355cec4f9d85a3b58dce36
Michael van Acken <mva@users.sf.net>
2002-01-03 14:09:39 +0100

    In IR and SSA: pass length as additional argument(s) to open array parameters.  When passing a string constant to such a parameter, the string length plus 1 is passed to the callee as the length.  Added rudimentary parameter list mangling in IR.Builder.NewCall.

--- tests/compile/src/Out.c
+++ tests/compile/src/Out.c
@@ -5,15 +5,15 @@
 void Out__Open() {
 }
 
-void Out__Char(unsigned char ch) {
+void Out__Char(OOC_CHAR8 ch) {
   putc(ch, stdout);
 }
 
-void Out__String(unsigned char s[]) {
+void Out__String(OOC_CHAR8 s[], OOC_LEN s_0d) {
   fputs(s, stdout);
 }
 
-void Out__Int(int i, int n) {
+void Out__Int(OOC_INT32 i, OOC_INT32 n) {
   fprintf(stdout, "%*i", n, i);
 }
 
