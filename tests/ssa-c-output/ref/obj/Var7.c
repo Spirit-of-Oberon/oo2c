7043889dcc32282365c6a86d0c3bd3aceaa49eb4
Michael van Acken <mva@users.sf.net>
2002-01-04 17:07:03 +0100

    Open array parameters are passed with length information, and local copies are created for value parameters.

--- tests/ssa-c-output/ref/obj/Var7.c
+++ tests/ssa-c-output/ref/obj/Var7.c
@@ -1,9 +1,14 @@
 #include "./Var7.d"
 #include "__oo2c.h"
 
-static OOC_INT16 Var7__Len(OOC_CHAR8 str[]) {
+static OOC_INT16 Var7__Len(OOC_CHAR8 str__ref[], OOC_LEN str_0d) {
   register OOC_INT32 i0,i1;
+  OOC_CHAR8 *str;
+  OOC_PUSH_VPAR_STACK
 
+  i0 = str_0d;
+  OOC_ALLOCATE_VPAR(str,OOC_CHAR8 ,i0)
+  OOC_INITIALIZE_VPAR(str__ref,str,OOC_CHAR8 ,i0)
   i0=-1;
 l1_loop:
   i0 = i0+1;
@@ -12,16 +17,17 @@ l1_loop:
   i1 = *(OOC_UINT8*)i1;
   i1 = i1=='\000';
   if (!i1) goto l1_loop;
+  OOC_POP_VPAR_STACK
   return i0;
 }
 
 void Var7__Test() {
   register OOC_INT32 i0;
 
-  i0 = Var7__Len("");
+  i0 = Var7__Len("", 1);
   Out__Int(i0, 0);
   Out__Ln();
-  i0 = Var7__Len("abc");
+  i0 = Var7__Len("abc", 4);
   Out__Int(i0, 0);
   Out__Ln();
   return;
--- tests/ssa-c-output/ref/obj/Var7b.c
+++ tests/ssa-c-output/ref/obj/Var7b.c
@@ -1,9 +1,14 @@
 #include "./Var7b.d"
 #include "__oo2c.h"
 
-static OOC_INT16 Var7b__Len(OOC_CHAR8 str[]) {
+static OOC_INT16 Var7b__Len(OOC_CHAR8 str__ref[], OOC_LEN str_0d) {
   register OOC_INT32 i0,i1;
+  OOC_CHAR8 *str;
+  OOC_PUSH_VPAR_STACK
 
+  i0 = str_0d;
+  OOC_ALLOCATE_VPAR(str,OOC_CHAR8 ,i0)
+  OOC_INITIALIZE_VPAR(str__ref,str,OOC_CHAR8 ,i0)
   i0=-1;
 l1_loop:
   i0 = i0+1;
@@ -13,16 +18,17 @@ l1_loop:
   i1 = *(OOC_UINT8*)i1;
   i1 = i1=='\000';
   if (!i1) goto l1_loop;
+  OOC_POP_VPAR_STACK
   return i0;
 }
 
 void Var7b__Test() {
   register OOC_INT32 i0;
 
-  i0 = Var7b__Len("");
+  i0 = Var7b__Len("", 1);
   Out__Int(i0, 0);
   Out__Ln();
-  i0 = Var7b__Len("abc");
+  i0 = Var7b__Len("abc", 4);
   Out__Int(i0, 0);
   Out__Ln();
   return;
