b457c0412ff0a27f739d033adff38ad14e7dddc1
Michael van Acken <mva@users.sf.net>
2002-01-06 09:50:41 +0100

    extended alias analysis to deal with VAR parameters

---
+++ tests/ssa-c-output/ref/obj/VarPar1b.c
@@ -0,0 +1,27 @@
+#include "./VarPar1b.d"
+#include "__oo2c.h"
+
+static OOC_INT16 VarPar1b__F(OOC_INT16 *p) {
+  register OOC_INT32 i0;
+
+  VarPar1b__x = 1;
+  *p = 2;
+  i0 = VarPar1b__x;
+  i0 = i0+2;
+  return i0;
+}
+
+void VarPar1b__Test() {
+  register OOC_INT32 i0;
+
+  i0 = VarPar1b__F((void*)(OOC_INT32)&VarPar1b__a);
+  Out__Int(i0, 0);
+  Out__Ln();
+  i0 = VarPar1b__F((void*)(OOC_INT32)&VarPar1b__x);
+  Out__Int(i0, 0);
+  Out__Ln();
+  return;
+}
+
+void VarPar1b_init(void) {
+}
---
+++ tests/ssa-c-output/ref/obj/VarPar1c.c
@@ -0,0 +1,23 @@
+#include "./VarPar1c.d"
+#include "__oo2c.h"
+
+static OOC_INT16 VarPar1c__F(OOC_INT16 *p) {
+  register OOC_INT32 i0;
+
+  i0 = 1+2;
+  VarPar1c__x = 1;
+  *p = 2;
+  return i0;
+}
+
+void VarPar1c__Test() {
+  register OOC_INT32 i0;
+
+  i0 = VarPar1c__F((void*)(OOC_INT32)&VarPar1c__a);
+  Out__Int(i0, 0);
+  Out__Ln();
+  return;
+}
+
+void VarPar1c_init(void) {
+}
