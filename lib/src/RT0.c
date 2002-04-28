b12ffd856039ba883ed96e8d9823272e54ed8c1d
Michael van Acken <mva@users.sf.net>
2002-04-28 14:34:16 +0200

    began data structure for meta data for the run-time system

---
+++ lib/src/RT0.c
@@ -0,0 +1,6 @@
+#include "RT0.d"
+
+#include "__oo2c.c"
+
+void RT0_init() {
+}
--- lib/src/__oo2c.c
+++ lib/src/__oo2c.c
@@ -17,7 +17,6 @@
     59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-#include "RT0.d"
 #include "__oo2c.h"
 #include <stdio.h>
 
