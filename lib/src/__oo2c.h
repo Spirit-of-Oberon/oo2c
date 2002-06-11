b5c0b667144570a605086e9cf329e2fb1fbcad7e
Michael van Acken <mva@users.sf.net>
2002-06-11 23:34:35 +0200

    add opcode for SYSTEM.LSH

--- lib/src/__oo2c.h
+++ lib/src/__oo2c.h
@@ -89,6 +89,11 @@ extern OOC_INT32 _cmp16(const OOC_CHAR8* l, const OOC_CHAR8* r);
 #define _ashr(_x,_n) (_x >> _n) | ((_x >= 0) ? 0 : ~(~(OOC_INT32)0 >> _n))
 #define _ash(_x,_n) (_n >= 0) ? _ashl(_x,_n) : _ashr(_x,- _n)
 
+/* SYSTEM.LSH(x,n) */
+#define _lshl(_x,_n,_type) ((_type) _x << _n)
+#define _lshr(_x,_n,_type) ((_type) _x >> _n)
+#define _lsh(_type,_x,_n) ((_n >= 0) ? _lshl(_x,_n,_type) : _lshr(_x,- _n,_type))
+
 /* CAP(CHAR) and CAP(LONGCHAR) */
 #define _cap(_c) ((96<_c && _c<123) || (224<=_c && _c<255 && _c!=247)) ? (_c-32) : _c
 #define _capl(_c) _cap(_c)
