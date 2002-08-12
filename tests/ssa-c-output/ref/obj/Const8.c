32490e12e802ff115d99d068f5bb6fb5d07448c9
Michael van Acken <mva@users.sf.net>
2002-08-12 18:50:07 +0200

    add unreachable code elimination tests for conditional operators & and OR

---
+++ tests/ssa-c-output/ref/obj/Const8.c
@@ -0,0 +1,64 @@
+#include "./Const8.d"
+#include "__oo2c.h"
+
+static void Const8__Tand(OOC_CHAR8 b) {
+  register OOC_INT32 i0;
+
+  i0 = b;
+  if (i0) goto l3;
+  Out0__String("FALSE ", 7);
+  goto l4;
+l3:
+  Out0__String("TRUE ", 6);
+l4:
+  return;
+}
+
+static void Const8__Fand(OOC_CHAR8 b) {
+
+  Out0__String("FALSE ", 7);
+  return;
+}
+
+static void Const8__Tor(OOC_CHAR8 b) {
+
+  Out0__String("TRUE ", 6);
+  return;
+}
+
+static void Const8__For(OOC_CHAR8 b) {
+  register OOC_INT32 i0;
+
+  i0 = b;
+  if (i0) goto l3;
+  Out0__String("FALSE ", 7);
+  goto l4;
+l3:
+  Out0__String("TRUE ", 6);
+l4:
+  return;
+}
+
+void Const8__Test() {
+
+  Const8__Tand(OOC_TRUE);
+  Const8__Tand(OOC_FALSE);
+  Out0__Ln();
+  Const8__Fand(OOC_TRUE);
+  Const8__Fand(OOC_FALSE);
+  Out0__Ln();
+  Const8__Tor(OOC_TRUE);
+  Const8__Tor(OOC_FALSE);
+  Out0__Ln();
+  Const8__For(OOC_TRUE);
+  Const8__For(OOC_FALSE);
+  Out0__Ln();
+  return;
+}
+
+void Const8_init(void) {
+
+  return;
+}
+
+/* --- */
