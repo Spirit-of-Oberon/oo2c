92bcea6396e685960565c8e0a53ecc40ed1e542a
Michael van Acken <mva@users.sf.net>
2002-10-14 23:53:59 +0200

    Add modifier flags NO_COPY and NIL_COMPAT to parameter declarations. Only NO_COPY is fully supported, and has been added to the test suite. I had to back out enforcement of the read-only status of NO_COPY parameters, because the standard lib passes such parameters to `VAR x: ARRAY OF BYTE' parameters.

---
+++ tests/ssa-c-output/ref/obj/NoCopy8.c
@@ -0,0 +1,16 @@
+#include "./NoCopy8.d"
+#include "__oo2c.h"
+
+static void NoCopy8__P(const struct NoCopy8__R *a) {
+
+  return;
+  ;
+}
+
+void NoCopy8_init(void) {
+
+  return;
+  ;
+}
+
+/* --- */
