ddbb0a9bf01e329b86d955a5e5190bb0e619b678
Michael van Acken <mva@users.sf.net>
2002-11-17 14:56:20 +0100

    Implement NO_LENGTH_INFO for open array types.

---
+++ tests/ssa-c-output/ref/obj/NoLengthInfo1.c
@@ -0,0 +1,13 @@
+#include "./NoLengthInfo1.d"
+#include "__oo2c.h"
+
+void NoLengthInfo1_init(void) {
+  register OOC_INT32 i0;
+
+  i0 = (OOC_INT32)NoLengthInfo1__a;
+  *(OOC_UINT8*)(_check_pointer(i0, 108)) = (OOC_CHAR8)'\000';
+  return;
+  ;
+}
+
+/* --- */
