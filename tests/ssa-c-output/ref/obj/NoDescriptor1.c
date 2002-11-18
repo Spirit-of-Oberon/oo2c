608869ba5e48a4f3e5aa69b90a1b49f505e1202c
Michael van Acken <mva@users.sf.net>
2002-11-18 01:28:30 +0100

    Implement flags ABSTRACT, NO_DESCRIPTOR, and NOT_EXTENSIBLE.

---
+++ tests/ssa-c-output/ref/obj/NoDescriptor1.c
@@ -0,0 +1,24 @@
+#include "./NoDescriptor1.d"
+#include "__oo2c.h"
+
+static void NoDescriptor1__P(struct NoDescriptor1__R *r) {
+
+  return;
+  ;
+}
+
+void NoDescriptor1__Test() {
+  struct NoDescriptor1__R r;
+
+  NoDescriptor1__P((void*)(OOC_INT32)&r);
+  return;
+  ;
+}
+
+void NoDescriptor1_init(void) {
+
+  return;
+  ;
+}
+
+/* --- */
