d5acc138d79498196febf45fee13d71beccf3f6c
Michael van Acken <mva@users.sf.net>
2003-02-14 21:09:00 +0100

    New test for extended dead code elimination.

---
+++ tests/ssa-c-output/ref/obj/Dead1.c
@@ -0,0 +1,24 @@
+#include <Dead1.d>
+#include <__oo2c.h>
+#include <setjmp.h>
+
+static void Dead1__P() {
+  register OOC_INT32 i0,i1;
+
+  i0=0;
+l1_loop:
+  i0 = i0+1;
+  i1 = i0<=9;
+  if (i1) goto l1_loop;
+l5:
+  return;
+  ;
+}
+
+void OOC_Dead1_init(void) {
+
+  return;
+  ;
+}
+
+/* --- */
