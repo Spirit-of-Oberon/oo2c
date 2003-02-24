fb3a42f02e50bbad7ce871f4e07429a50ecc09e8
Michael van Acken <mva@users.sf.net>
2003-02-24 05:40:05 +0100

    Register names changed after tuning in allocator.

--- tests/ssa-c-output/ref/obj/Quicksort1.c
+++ tests/ssa-c-output/ref/obj/Quicksort1.c
@@ -9,55 +9,53 @@ void Quicksort1__Sort(OOC_INT32 a[], OOC_LEN a_0d, OOC_INT32 m, OOC_INT32 n) {
   i1 = m;
   i2 = i0>i1;
   if (!i2) goto l18;
-  i2 = i1-1;
-  i3 = *(OOC_INT32*)((OOC_INT32)a+i0*4);
+  i2 = *(OOC_INT32*)((OOC_INT32)a+i0*4);
+  i3 = i1-1;
   i4=i0;
 l3_loop:
   
 l4_loop:
-  i2 = i2+1;
-  i5 = *(OOC_INT32*)((OOC_INT32)a+i2*4);
-  i5 = i5>=i3;
+  i3 = i3+1;
+  i5 = *(OOC_INT32*)((OOC_INT32)a+i3*4);
+  i5 = i5>=i2;
   if (!i5) goto l4_loop;
 l9_loop:
   i4 = i4-1;
   i5 = *(OOC_INT32*)((OOC_INT32)a+i4*4);
-  i5 = i5<=i3;
+  i5 = i5<=i2;
   if (!i5) goto l9_loop;
 l13:
-  i5 = i2>=i4;
+  i5 = i3>=i4;
   if (i5) goto l17;
-  i5 = *(OOC_INT32*)((OOC_INT32)a+i2*4);
+  i5 = *(OOC_INT32*)((OOC_INT32)a+i3*4);
   i6 = *(OOC_INT32*)((OOC_INT32)a+i4*4);
-  *(OOC_INT32*)((OOC_INT32)a+i2*4) = i6;
+  *(OOC_INT32*)((OOC_INT32)a+i3*4) = i6;
   *(OOC_INT32*)((OOC_INT32)a+i4*4) = i5;
   
   goto l3_loop;
 l17:
-  i3 = *(OOC_INT32*)((OOC_INT32)a+i0*4);
-  i5 = *(OOC_INT32*)((OOC_INT32)a+i2*4);
-  *(OOC_INT32*)((OOC_INT32)a+i2*4) = i3;
+  i2 = *(OOC_INT32*)((OOC_INT32)a+i0*4);
+  i5 = *(OOC_INT32*)((OOC_INT32)a+i3*4);
+  *(OOC_INT32*)((OOC_INT32)a+i3*4) = i2;
   *(OOC_INT32*)((OOC_INT32)a+i0*4) = i5;
   Quicksort1__Sort((void*)(OOC_INT32)a, a_0d, i1, i4);
-  Quicksort1__Sort((void*)(OOC_INT32)a, a_0d, (i2+1), i0);
+  Quicksort1__Sort((void*)(OOC_INT32)a, a_0d, (i3+1), i0);
 l18:
   return;
   ;
 }
 
 void Quicksort1__Test() {
-  register OOC_INT32 i0,i1,i2;
+  register OOC_INT32 i0,i1;
   OOC_INT32 a[64];
 
   i0=0;
 l1_loop:
   i1 = RandomNumbers__RND(64);
-  i2 = i0+1;
   *(OOC_INT32*)((OOC_INT32)a+i0*4) = i1;
-  i0 = i2<=63;
-  if (!i0) goto l5;
-  i0=i2;
-  goto l1_loop;
+  i0 = i0+1;
+  i1 = i0<=63;
+  if (i1) goto l1_loop;
 l5:
   Quicksort1__Sort((void*)(OOC_INT32)a, 64, 0, 63);
   i0=0;
--- tests/ssa-c-output/ref/obj/Var8.c
+++ tests/ssa-c-output/ref/obj/Var8.c
@@ -3,18 +3,16 @@
 #include <setjmp.h>
 
 static void Var8__Zero(OOC_CHAR8 str[], OOC_LEN str_0d) {
-  register OOC_INT32 i0,i1,i2;
+  register OOC_INT32 i0,i1;
 
   i0 = 0<str_0d;
   if (!i0) goto l8;
   i0=0;
 l3_loop:
-  i1 = i0+1;
-  i2 = i1<str_0d;
   *(OOC_UINT8*)((OOC_INT32)str+(_check_index(i0, str_0d, OOC_UINT32, 148))) = 0u;
-  if (!i2) goto l8;
-  i0=i1;
-  goto l3_loop;
+  i0 = i0+1;
+  i1 = i0<str_0d;
+  if (i1) goto l3_loop;
 l8:
   return;
   ;
