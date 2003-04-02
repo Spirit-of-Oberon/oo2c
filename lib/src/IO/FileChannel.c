e2286a83226e27ee5f2d8dfd5f937e1389f099a7
Stewart Greenhill <sgreenhill@users.sf.net>
2003-04-02 11:05:48 +0200

    For MINGW, Removed redundant call to access().

--- lib/src/IO/FileChannel.c
+++ lib/src/IO/FileChannel.c
@@ -223,11 +223,9 @@ void IO_FileChannel__ChannelDesc_CloseAndRegister(IO_FileChannel__Channel ch) {
       char* tname = (char*)OOC_METHOD(ch->tmpName,Object__String8Desc_CharsLatin1)(ch->tmpName);
 #ifdef __MINGW32__
 /* Windows doesn't allow files to be renamed over existing files. Therefore,
- * we first remove the destination file if it exists. */
+ * we first remove the destination file. */
 
-      if (access(fname, 0)==0) {
         res = unlink(fname);
-      }
 #endif
       res = rename(tname, fname);
       remove_tmp_file(ch);
