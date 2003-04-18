ea197aaf49b930a5900002fc055216b3cb61ac50
Michael van Acken <mva@users.sf.net>
2003-04-18 23:29:21 +0200

    Generics: Add parameterized type to symbol table.

--- tests/symtab/wf-resolve-names.sh
+++ tests/symtab/wf-resolve-names.sh
@@ -4,6 +4,7 @@ err_count=0
 for i in wf/src/*.Mod; do
   module=`basename $i .Mod`
   rm -f "wf/oocdoc/xml/${module}_symtab.xml"
+  #echo $TEST_PROG -r wf --resolve-names-xml "$module"
   if $TEST_PROG -r wf --resolve-names-xml "$module" >/dev/null; then
     if $DIFF "wf/out-xml/resolve-names/${module}_symtab.xml" "wf/oocdoc/xml/${module}_symtab.xml"; then
       echo "ok: $i"
