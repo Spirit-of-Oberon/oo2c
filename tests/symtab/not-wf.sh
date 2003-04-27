65193e80377262e9c6feace5fbaebc88c80f9009
Michael van Acken <mva@users.sf.net>
2003-04-28 00:19:13 +0200

    *** empty log message ***

--- tests/symtab/not-wf.sh
+++ tests/symtab/not-wf.sh
@@ -4,6 +4,7 @@ err_count=0
 for i in not-wf/src/*.Mod; do
   module=`basename $i .Mod`
   rm -f "wf/oocdoc/xml/${module}_symtab.xml" test-output/not-wf/$module.err
+  #echo $TEST_PROG -r not-wf --resolve-names-xml "$module"
   if $TEST_PROG -r not-wf --resolve-names-xml "$module" >/dev/null 2>test-output/not-wf/$module.err; then
     err_count=`expr $err_count + 1`
     echo "failed: $i (exit code is zero)"
