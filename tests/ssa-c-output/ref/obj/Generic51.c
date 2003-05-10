0748522d6977e8f693a7db5e833219f7d668d4c8
Michael van Acken <mva@users.sf.net>
2003-05-10 22:41:26 +0200

    Regression test for wrong pointer aliasing decision.

---
+++ tests/ssa-c-output/ref/obj/Generic51.c
@@ -0,0 +1,33 @@
+#include <Generic51.d>
+#include <__oo2c.h>
+#include <setjmp.h>
+
+Generic51__Table Generic51__DictionaryDesc_Resize(Generic51__Dictionary dict) {
+  register OOC_INT32 i0,i1;
+
+  i0 = (OOC_INT32)dict;
+  i0 = i0+4;
+  i1 = (OOC_INT32)*(OOC_INT32*)i0;
+  *(OOC_INT32*)i0 = ((OOC_INT32)RT0__NewObject(_td_Generic51__Table.baseTypes[0], 123));
+  return (Generic51__Table)i1;
+  ;
+}
+
+void Generic51__Test(void) {
+  register OOC_INT32 i0;
+
+  i0 = (OOC_INT32)RT0__NewObject(_td_Generic51__Dictionary.baseTypes[0]);
+  *(OOC_INT32*)(i0+4) = (OOC_INT32)0;
+  i0 = (OOC_INT32)Generic51__DictionaryDesc_Resize((Generic51__Dictionary)i0);
+  _assert((i0==(OOC_INT32)0), 127, 868);
+  return;
+  ;
+}
+
+void OOC_Generic51_init(void) {
+
+  return;
+  ;
+}
+
+/* --- */
