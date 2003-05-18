957d9fc5c1d452cb3ab94ac3f40e09133f8e973f
Michael van Acken <mva@users.sf.net>
2003-05-18 17:59:19 +0200

    (OS:Files) Change error reporting to use exceptions.

--- lib/src/OS/Files.c
+++ lib/src/OS/Files.c
@@ -18,74 +18,9 @@
 
 #define BUFFER_SIZE 1024
 
-void OS_Files__ErrorContextDesc_GetTemplate(OS_Files__ErrorContext context, Msg__Msg msg, Msg__LString templ, OOC_LEN templ_0d) {
-  char *str;
-  OOC_INT32 res = msg->code;
-  
-  if (res == OS_Files__accessDenied) {
-    str = "No write permission for parent directory";
-  } else if (res == OS_Files__fileExists) {
-    str = "A file of this name already exists";
-  } else if (res == OS_Files__writeError) {
-    str = "Write error";
-  } else if (res == OS_Files__fileBusy) {
-    str = "File in use";
-  } else if (res == OS_Files__noSuchFile) {
-    str = "File does not exist";
-  } else {
-    str = "Unknown error code";
-  }
-
-  _copy_8to16(str, templ, templ_0d);
-  if (msg->attribList) {
-    Msg__Attribute attr;
-    OOC_CHAR16 eol[2] = {(OOC_CHAR16)CharClass__eol[0], (OOC_CHAR16)0};
-    OOC_CHAR16 str16[Msg__sizeAttrName+1];
-    
-    attr = msg->attribList;
-    while (attr) {
-      LongStrings__Append(eol, 2, templ, templ_0d);
-      _copy_8to16((char*)attr->name, str16, strlen((char*)attr->name)+1);
-      LongStrings__Append(str16, Msg__sizeAttrName+1, templ, templ_0d);
-      _copy_8to16("=${", str16, Msg__sizeAttrName+1);
-      LongStrings__Append(str16, Msg__sizeAttrName+1, templ, templ_0d);
-      _copy_8to16((char*)attr->name, str16, Msg__sizeAttrName+1);
-      LongStrings__Append(str16, Msg__sizeAttrName+1, templ, templ_0d);
-      _copy_8to16("}", str16, Msg__sizeAttrName+1);
-      LongStrings__Append(str16, Msg__sizeAttrName+1, templ, templ_0d);
-      attr = attr->nextAttrib;
-    }
-  }
-}
-
-
-static Msg__Msg get_error(const OS_Files__Path path) {
-  Msg__Msg msg;
-  OOC_INT32 code;
-  
-  switch (errno) {
-  case EACCES: code = OS_Files__accessDenied; break;
-  case EBUSY: code = OS_Files__fileBusy; break;
-  case EEXIST: code = OS_Files__fileExists; break;
-  case ENOENT: code = OS_Files__noSuchFile; break;
-  default: code = OS_Files__writeError;
-  }
-  
-  msg = Msg__New((Msg__Context)OS_Files__errorContext, code);
-  DYN_TBCALL(Msg,MsgDesc,SetStringAttrib,msg,
-	     (msg, (const OOC_CHAR8*)"path", 5, (OOC_CHAR8*)path));
-#if HAVE_STRERROR
-  DYN_TBCALL(Msg,MsgDesc,SetStringAttrib,msg,
-	     (msg, (const OOC_CHAR8*)"errstr", 7, (OOC_CHAR8*)strerror(errno)));
-#endif
-  DYN_TBCALL(Msg,MsgDesc,SetIntAttrib,msg,
-	     (msg, (const OOC_CHAR8*)"errno", 6, (OOC_INT32)errno));
-  return msg;
-}
-
 
 static void extend_result(OS_Files__NameArray *result, int *result_len,
-			  Object__String8 buffer[], int buffer_len) {
+			  Object__String buffer[], int buffer_len) {
   int len=*result_len+buffer_len;
   char* ptr;
   int i;
@@ -106,13 +41,13 @@ static void extend_result(OS_Files__NameArray *result, int *result_len,
 }
 
 #define DIRENT_BUFFER 1024
-OS_Files__NameArray OS_Files__listdir(const OS_Files__Path path__ref,
-				      OOC_LEN path_0d, Msg__Msg *res) {
+OS_Files__NameArray OS_Files__listdir(Object__String path) {
   DIR* dir;
-
-  dir = opendir(path__ref);
+  char* fname = OS_Path__Encode(path);
+  
+  dir = opendir(fname);
   if (dir != NULL) {
-    Object__String8 buffer[DIRENT_BUFFER];
+    Object__String buffer[DIRENT_BUFFER];
     OS_Files__NameArray result = NULL;
     int result_len = 0;
     struct dirent *de;
@@ -142,91 +77,73 @@ OS_Files__NameArray OS_Files__listdir(const OS_Files__Path path__ref,
 	  extend_result(&result, &result_len, buffer, i);
 	  i = 0;
 	}
-	len = strlen(de->d_name);
-	buffer[i] = Object__NewLatin1Region(de->d_name, len+1, 0, len);
+	buffer[i] = OS_Path__Decode(de->d_name, strlen(de->d_name)+1);
 	i++;
       }
     }
     closedir(dir);
 
-    *res = OS_Files__done;
     extend_result(&result, &result_len, buffer, i);
     return result;
   } else {
-    *res = get_error(Msg__GetStringPtr(path__ref, path_0d));
-    return NULL;
+    IO_StdChannels__IOError(path);
   }
 }
 
-void OS_Files__mkdir(const OS_Files__Path path, OOC_LEN path_0d,
-                     OS_Files__Mode mode, Msg__Msg *res) {
+void OS_Files__mkdir(Object__String path, OS_Files__Mode mode) {
   int rc;
+  char* fname = OS_Path__Encode(path);
 
 #ifdef __MINGW32__
-  rc = mkdir((const char*)path);
+  rc = mkdir(fname);
 #else
-  rc = mkdir((const char*)path, mode);
+  rc = mkdir(fname, mode);
 #endif
   if (rc) {
-    *res = get_error(Msg__GetStringPtr(path, path_0d));
-  } else {
-    *res = OS_Files__done;
+    IO_StdChannels__IOError(path);
   }
 }
 
-void OS_Files__makedirs(const OS_Files__Path path, OOC_LEN path_0d,
-                        OS_Files__Mode mode, Msg__Msg *res) {
+void OS_Files__makedirs(Object__String path, OS_Files__Mode mode) {
   int rc;
   struct stat sbuf;
+  char* fname = OS_Path__Encode(path);
   
-  if (path[0] == '\000') {	/* empty path --> no operation */
-    *res = OS_Files__done;
+  if (fname[0] == '\000') {	/* empty path --> no operation */
     return;
   }
   
-  rc = stat((const char*)path, &sbuf);
+  rc = stat(fname, &sbuf);
   if (rc) {			/* stat failure: file does not exist */
     OOC_CHAR8 ppath[BUFFER_SIZE];
     int i;
     int last = 0;
     
-    for (i = 0; (i < BUFFER_SIZE) && (path[i] != '\000'); i++) {
-      ppath[i] = path[i];
-      if ((path[i] == '/') && (i > 0) && (path[i-1] != '/')) {
+    for (i = 0; (i < BUFFER_SIZE) && (fname[i] != '\000'); i++) {
+      ppath[i] = fname[i];
+      if ((fname[i] == '/') && (i > 0) && (fname[i-1] != '/')) {
 	last = i;
       }
     }
     ppath[last] = '\000';
-    OS_Files__makedirs(ppath, BUFFER_SIZE, mode, res);
-    if (*res == OS_Files__done) {
-      OS_Files__mkdir(path, path_0d, mode, res);
-    }
+    OS_Files__makedirs(OS_Path__Decode(ppath, BUFFER_SIZE), mode);
+    OS_Files__mkdir(OS_Path__Decode(fname, OOC_ARRAY_LENGTH(fname,0)), mode);
     
-  } else {			/* stat successful: file exists */
-    if (S_ISDIR(sbuf.st_mode)) {
-      *res = OS_Files__done;	/* path exists and is directory */
-    } else {
-      /* path is not directory: let mkdir get the error message */
-      OS_Files__mkdir(path, path_0d, mode, res);
-    }
+  } else if (!S_ISDIR(sbuf.st_mode)) {
+    /* path is not directory: let mkdir get the error message */
+    OS_Files__mkdir(OS_Path__Decode(fname, OOC_ARRAY_LENGTH(fname,0)), mode);
   }
 }
 
-void OS_Files__remove(const OS_Files__Path path, OOC_LEN path_0d,
-                      Msg__Msg *res) {
+void OS_Files__remove(Object__String path) {
   int rc;
+  char* fname = OS_Path__Encode(path);
 
-  rc = unlink((const char*)path);
+  rc = unlink(fname);
   if (rc) {
-    *res = get_error(Msg__GetStringPtr(path, path_0d));
-  } else {
-    *res = OS_Files__done;
+    IO_StdChannels__IOError(path);
   }
 }
 
 void OOC_OS_Files_init(void) {
-  OS_Files__errorContext = 
-    RT0__NewObject(OOC_TYPE_DESCR(OS_Files,ErrorContextDesc));
-  Msg__InitContext((Msg__Context)OS_Files__errorContext, 
-		   (const OOC_CHAR8*)"OS:Files", 15);
 }
