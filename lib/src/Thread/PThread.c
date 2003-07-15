1ed3649bdcadcd7110b5cc783381e760f9abe9e2
Michael van Acken <mva@users.sf.net>
2003-07-15 22:15:52 +0200

    Rename module PThread to Thread:PThread.

--- lib/src/Thread/PThread.c
+++ lib/src/Thread/PThread.c
@@ -1,6 +1,6 @@
 #include <__oo2c.h>
 #include <__config.h>
-#include <PThread.d>
+#include <Thread/PThread.d>
 
 #include <errno.h>
 #include <stdio.h>
@@ -11,10 +11,10 @@
 #  include <gc/gc.h>
 #endif
 
-static PThread__Thread main_thread;
+static Thread_PThread__Thread main_thread;
 static pthread_key_t threadobj_key;
 
-void PThread__ErrorDesc_INIT(PThread__Error e, Object__String msg,
+void Thread_PThread__ErrorDesc_INIT(Thread_PThread__Error e, Object__String msg,
 			     OOC_INT32 errorCode) {
   Exception__ExceptionDesc_INIT((Exception__Exception)e, msg);
   e->errorCode = errorCode;
@@ -42,8 +42,8 @@ static void check_error(int errorCode, int fatal) {
       str = buffer;
     }
     
-    e = RT0__NewObject(OOC_TYPE_DESCR(PThread,ErrorDesc));
-    PThread__ErrorDesc_INIT((PThread__Error)e,
+    e = RT0__NewObject(OOC_TYPE_DESCR(Thread_PThread,ErrorDesc));
+    Thread_PThread__ErrorDesc_INIT((Thread_PThread__Error)e,
 			    (Object__String)Object__NewLatin1(str, 64),
 			    errorCode);
     if (fatal) {
@@ -56,30 +56,30 @@ static void check_error(int errorCode, int fatal) {
 
 
 
-void PThread__ThreadDesc_INIT(PThread__Thread t) {
+void Thread_PThread__ThreadDesc_INIT(Thread_PThread__Thread t) {
   Exception__InitThreadState(&t->exceptionState);
   t->thread = NULL;
 }
 
-void PThread__ThreadDesc_Run(PThread__Thread t) {
+void Thread_PThread__ThreadDesc_Run(Thread_PThread__Thread t) {
   /* abstract method */
 }
 
-void PThread__ThreadDesc_RunWrapper(PThread__Thread t) {
+void Thread_PThread__ThreadDesc_RunWrapper(Thread_PThread__Thread t) {
   /* make the thread's object available as thread specific data */
   pthread_setspecific(threadobj_key, t);
   
   /* because the thread's stack of exception handlers is cleared, any
      exception raised by the Run() method will end up in the default
      handler of Exception.ActivateContext() */
-  OOC_METHOD(t, PThread__ThreadDesc_Run)(t);
+  OOC_METHOD(t, Thread_PThread__ThreadDesc_Run)(t);
 }
 
-void PThread__ThreadDesc_Start(PThread__Thread t) {
+void Thread_PThread__ThreadDesc_Start(Thread_PThread__Thread t) {
   sigset_t oldmask, newmask;
   int rc;
   void* (*start)(void*) =
-    (void*(*)(void*))OOC_METHOD(t, PThread__ThreadDesc_RunWrapper);
+    (void*(*)(void*))OOC_METHOD(t, Thread_PThread__ThreadDesc_RunWrapper);
   
   /* Mask all signals in the current thread before creating the new
    * thread.  This causes the new thread to start with all signals
@@ -92,58 +92,58 @@ void PThread__ThreadDesc_Start(PThread__Thread t) {
   check_error(rc, OOC_FALSE);
 }
 
-void PThread__ThreadDesc_Join(PThread__Thread t) {
+void Thread_PThread__ThreadDesc_Join(Thread_PThread__Thread t) {
   void *exit_code;
   
   check_error(pthread_join((pthread_t)t->thread, &exit_code), OOC_FALSE);
 }
 
-#define THIS_THREAD ((PThread__Thread)pthread_getspecific(threadobj_key))
-PThread__Thread PThread__CurrentThread() {
+#define THIS_THREAD ((Thread_PThread__Thread)pthread_getspecific(threadobj_key))
+Thread_PThread__Thread Thread_PThread__CurrentThread() {
   return THIS_THREAD;
 }
 
 
 
-void PThread__MutexDesc_INIT(PThread__Mutex m) {
+void Thread_PThread__MutexDesc_INIT(Thread_PThread__Mutex m) {
   m->mutex = RT0__NewBlock(sizeof(pthread_mutex_t));
   pthread_mutex_init((pthread_mutex_t*)m->mutex, NULL);  /* always returns 0 */
 }
 
-void PThread__MutexDesc_Lock(PThread__Mutex m) {
+void Thread_PThread__MutexDesc_Lock(Thread_PThread__Mutex m) {
   check_error(pthread_mutex_lock((pthread_mutex_t*)m->mutex), OOC_TRUE);
 }
 
-void PThread__MutexDesc_Unlock(PThread__Mutex m) {
+void Thread_PThread__MutexDesc_Unlock(Thread_PThread__Mutex m) {
   check_error(pthread_mutex_unlock((pthread_mutex_t*)m->mutex), OOC_TRUE);
 }
 
-void PThread__MutexDesc_Destroy(PThread__Mutex m) {
+void Thread_PThread__MutexDesc_Destroy(Thread_PThread__Mutex m) {
   check_error(pthread_mutex_destroy((pthread_mutex_t*)m->mutex), OOC_TRUE);
   m->mutex = NULL;
 }
 
 
 
-void PThread__ConditionDesc_INIT(PThread__Condition c) {
+void Thread_PThread__ConditionDesc_INIT(Thread_PThread__Condition c) {
   c->cond = RT0__NewBlock(sizeof(pthread_cond_t));
   pthread_cond_init((pthread_cond_t*)c->cond, NULL);  /* always returns 0 */
 }
 
-void PThread__ConditionDesc_Signal(PThread__Condition c) {
+void Thread_PThread__ConditionDesc_Signal(Thread_PThread__Condition c) {
   pthread_cond_signal((pthread_cond_t*)c->cond);  /* always returns 0 */
 }
 
-void PThread__ConditionDesc_Broadcast(PThread__Condition c) {
+void Thread_PThread__ConditionDesc_Broadcast(Thread_PThread__Condition c) {
   pthread_cond_broadcast((pthread_cond_t*)c->cond);  /* always returns 0 */
 }
 
-void PThread__ConditionDesc_Wait(PThread__Condition c, PThread__Mutex m) {
+void Thread_PThread__ConditionDesc_Wait(Thread_PThread__Condition c, Thread_PThread__Mutex m) {
   pthread_cond_wait((pthread_cond_t*)c->cond, (pthread_mutex_t*)m->mutex);
   /* always returns 0 */
 }
 
-void PThread__ConditionDesc_Destroy(PThread__Condition c) {
+void Thread_PThread__ConditionDesc_Destroy(Thread_PThread__Condition c) {
   check_error(pthread_cond_destroy((pthread_cond_t*)c->cond), OOC_TRUE);
   c->cond = NULL;
 }
@@ -154,11 +154,11 @@ static Exception__ThreadStatePtr tls_GetThreadState() {
   return &THIS_THREAD->exceptionState;
 }
 
-void OOC_PThread_init() {
+void OOC_Thread_PThread_init() {
   pthread_key_create(&threadobj_key, NULL);
   Exception__GetThreadState = tls_GetThreadState;
   
-  main_thread = RT0__NewObject(OOC_TYPE_DESCR(PThread,ThreadDesc));
-  PThread__ThreadDesc_INIT(main_thread);
+  main_thread = RT0__NewObject(OOC_TYPE_DESCR(Thread_PThread,ThreadDesc));
+  Thread_PThread__ThreadDesc_INIT(main_thread);
   pthread_setspecific(threadobj_key, main_thread);
 }
