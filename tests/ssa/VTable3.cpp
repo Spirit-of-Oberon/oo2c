171f1636f3a816c4f932596082f07e3d893d8178
Stewart Greenhill <sgreenhill@users.sf.net>
2003-09-12 10:12:39 +0200

    Added tests for VTABLE flag. This includes a foreign implementation of an object using C++ classes. Tests VTable3 and VTable5 are marked "informal" as they rely on "libstdc++" which may not always be available.

---
+++ tests/ssa/VTable3.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+class T {
+public:
+    T() { }
+
+    virtual void String(char * str, int length) {
+        printf("%s", str);
+    }
+
+    virtual void Int(int a) {
+        printf("%d", a); fflush(stdout);
+    }
+
+    virtual void Ln() {
+        printf("\n"); fflush(stdout);
+    }
+
+    virtual void Chain(T * obj, int a) {
+        printf("VTable3:T %d\n", a); fflush(stdout);
+        if (a > 0)
+            obj->Chain(this, a-1);
+    }
+};
+
+extern "C" T * VTable3__NewT(void) {
+    return new T();
+}
+
+extern "C" void OOC_VTable3_init0(void) {
+}
