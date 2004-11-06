4b483843f5e56a4c6e8a2adb04490e01bd6df92d
Michael van Acken <mva@users.sf.net>
2004-11-06 23:45:40 +0100

    more tests

--- rsrc/OOC/emacs/run-tests.sh
+++ rsrc/OOC/emacs/run-tests.sh
@@ -4,5 +4,5 @@ FILES=tests/*.Mod
 rm -f tests/*.indent
 emacs -batch -q -l ./o2-indent.el -f ob2-indent-all-buffers $FILES
 for i in $FILES; do
-  diff -u `dirname $i`/`basename $i .Mod`.reference `dirname $i`/`basename $i .Mod`.indent
+  diff -u $i `dirname $i`/`basename $i .Mod`.indent
 done
